a6aed1fd41f6c334e05076b52c28d3f431e05e68
Sergey <cydoor88@gmail.com>
2024-08-11 00:20:31 +0300

    add images generating

--- bot/bot.py
+++ bot/bot.py
@@ -39,25 +39,22 @@ logger = logging.getLogger(__name__)
 user_semaphores = {}
 user_tasks = {}
 
-HELP_MESSAGE = """Commands:
-⚪ /retry – Regenerate last bot answer
-⚪ /new – Start new dialog
-⚪ /mode – Select chat mode
-⚪ /settings – Show settings
+HELP_MESSAGE = """Поддерживаемые команды:
+⚪ /retry – Повторить запрос
+⚪ /new – Создать новый диалог
+⚪ /mode – Выбор режима
+⚪ /settings – Настройки
 ⚪ /balance – Show balance
-⚪ /help – Show help
+⚪ /help – Помощь
 
-🎨 Generate images from text prompts in <b>👩‍🎨 Artist</b> /mode
-👥 Add bot to <b>group chat</b>: /help_group_chat
-🎤 You can send <b>Voice Messages</b> instead of text
+🎨 Поддерживается генерация изображений в режиме <b>👩‍🎨 Artist</b> /mode
+🎤 Поддерживается отправка голосовых сообщений!
 """
 
-HELP_GROUP_CHAT_MESSAGE = """You can add bot to any <b>group chat</b> to help and entertain its participants!
-
-Instructions (see <b>video</b> below):
-1. Add the bot to the group chat
-2. Make it an <b>admin</b>, so that it can see messages (all other rights can be restricted)
-3. You're awesome!
+HELP_GROUP_CHAT_MESSAGE = """Данный бот может быть добавлены в любую TG-группу:
+1. Добавьте бота в группу
+2. Сделайте его <b>Администратором</b>, но уберите все права.
+3. Вы молодец!
 
 To get a reply from the bot in the chat – @ <b>tag</b> it or <b>reply</b> to its message.
 For example: "{bot_username} write a poem about Telegram"
